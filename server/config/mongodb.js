b97ea7047b0de50740c4cda201dd456282fbabfb
DeepakMahto99999 <mahtodeepak239@gmail.com>
2026-02-26 17:30:57 +0530

    in mongodb added central dubugging

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,14 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import logger from "../utils/logger.js";
 
 const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    logger.error("MONGODB_URI is not defined in environment variables");
+    process.exit(1);
+  }
+
   try {
-    mongoose.connection.on('connected', () => {
-      console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    logger.info("MongoDB connected successfully");
+
+    mongoose.connection.on("error", (err) => {
+      logger.error("MongoDB runtime error", { message: err.message });
     });
 
-    await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connection.on("disconnected", () => {
+      logger.warn("MongoDB disconnected");
+    });
+
   } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+    logger.error("MongoDB connection failed", {
+      message: error.message,
+      stack: error.stack
+    });
+
     process.exit(1);
   }
 };
